0b6812cebe6bd11e7c7820dbaaf649a6508160e7
ellroberts <elliot.roberts@gmail.com>
2025-06-01 18:12:13 +0100

    added 3 MS types

--- src/components/LeftPanel.jsx
+++ src/components/LeftPanel.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { usePlan } from "./PlanContext";
 import ItemGroup from "./ItemGroup";
@@ -10,6 +11,26 @@ const data = [
       { id: "1", term: "Monthly", billing: "Monthly", price: 7, min: 10 },
       { id: "2", term: "Monthly", billing: "Annual", price: 7, min: 0 },
       { id: "3", term: "Annual", billing: "Annual", price: 7, min: 0 },
+    ],
+  },
+  {
+    name: "MS365 Business Standard",
+    id: "standard",
+    options: [
+      { id: "standard-1", term: "Monthly", billing: "Monthly", price: 7, min: 0 },
+      { id: "standard-2", term: "Monthly", billing: "Annual", price: 7, min: 0 },
+      { id: "standard-3", term: "Annual", billing: "Monthly", price: 7, min: 0 },
+      { id: "standard-4", term: "Annual", billing: "Annual", price: 7, min: 0 },
+    ],
+  },
+  {
+    name: "MS365 Business Premium",
+    id: "premium",
+    options: [
+      { id: "premium-1", term: "Monthly", billing: "Monthly", price: 7, min: 0 },
+      { id: "premium-2", term: "Monthly", billing: "Annual", price: 7, min: 0 },
+      { id: "premium-3", term: "Annual", billing: "Monthly", price: 7, min: 0 },
+      { id: "premium-4", term: "Annual", billing: "Annual", price: 7, min: 0 },
     ],
   },
   {
