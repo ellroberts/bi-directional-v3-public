dcf60aaf865b0895b0812d5eb15fdc0cee2e2534
ellroberts <elliot.roberts@gmail.com>
2025-06-01 08:32:08 +0100

    tested tailwind colours on all files

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
 import React from "react";
-import { PlanProvider } from './components/PlanContext';
-import OptionRow from './components/OptionRow';
 import LeftPanel from "./components/LeftPanel";
+import RightPanel from "./components/RightPanel";
+import { PlanProvider } from "./components/PlanContext";
 
 export default function App() {
   return (
     <PlanProvider>
-      <div className="p-6 space-y-6">
-        <h1 className="text-2xl font-bold text-indigo-600">
-          Testing OptionRow 3.0
-        </h1>
-        <OptionRow
-          groupId="test-group"
-          option={{
-            id: 'opt-1',
-            name: 'Sample Option',
-            price: 9.99,
-            qty: 3,
-            min: 1,
-          }}
-        />
+      <div className="max-w-[1200px] mx-auto px-4 py-6">
+        <div className="flex flex-col md:flex-row gap-6">
+          <div className="w-full md:w-2/3">
+            <LeftPanel />
+          </div>
+          <div className="w-full md:w-1/3 bg-gray-100 p-4 rounded-md shadow-sm">
+            <RightPanel />
+          </div>
+        </div>
       </div>
     </PlanProvider>
   );
 }
-
